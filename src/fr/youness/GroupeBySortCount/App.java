bfb3fcec7353d2c907264981faffcbb9b131f1cf
ychafia <chafiayouness@gmail.com>
2017-08-13 18:28:35 +0200

    trie test

--- src/fr/youness/GroupeBySortCount/App.java
+++ src/fr/youness/GroupeBySortCount/App.java
@@ -1,6 +1,8 @@
 package fr.youness.GroupeBySortCount;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -9,12 +11,21 @@ import java.util.stream.Collectors;
 public class App {
 
 	public static void main(String[] args) {
-		List<String> mesSports = Arrays.asList("Foot", "Tennis", "Rugby", "Foot", 
+		List<String> mesSports = Arrays.asList("Foot", "Tennis", "Rugby", "Tennis", 
 				"Natation", "Volley", "Regby", "Foot", "Athlétisme", "Handball");
 		
 		Map<String, Long> result = mesSports.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-		
+		System.out.println("GroupBy : ");
 		System.out.println(result);
+		
+		System.out.println("* * * * * * * * * *  * *");
+		System.out.println("Trier : ");
+		
+		Map<String, Long> resultFinal = new LinkedHashMap();
+		result.entrySet().stream().sorted(Map.Entry.<String, Long>comparingByValue()
+                .reversed()).forEachOrdered(e -> resultFinal.put(e.getKey(), e.getValue()));
+		
+		System.out.println(resultFinal);
 	}
 
 }
